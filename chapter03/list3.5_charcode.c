2bc355f7b9111e6d145ef18095da6850ff28393d
ciscofort <ciscofort@gmail.com>
2013-01-16 21:07:31 +0800

    第3章 数据和C 程序清单3.5 - 3.6

---
+++ chapter03/list3.5_charcode.c
@@ -0,0 +1,13 @@
+/* charcode.c -- 显示一个字符的编码值 */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+	
+	printf("Please enter a character:\n");
+	scanf("%c", &ch);	// 用户输入字符
+	printf("The code for %c is %d.\n", ch, ch);
+	
+	return 0;
+}
---
+++ chapter03/list3.6_altnames.c
@@ -0,0 +1,19 @@
+/* altnames.c -- 可移植的整数类型名 */
+#include <stdio.h>
+#include <inttypes.h>	// 支持可移植类型
+
+int main(void)
+{
+	int16_t me16;	// me16 是一个 16 位有符号变量
+	
+	me16 = 4593;
+	
+	printf("First, assume int16_t is short: ");
+	printf("me16 = %hd\n", me16);
+	printf("Next, let's not make any assumptions.\n");
+	printf("Instead, use a \"macro\" from inttypes.h: ");
+	printf("me16 = %" PRId16 "\n", me16);
+	
+	getchar();
+	return 0;
+}
