b0ed63406c789db682ff5416c3d5d048d92f340e
ciscofort <ciscofort@gmail.com>
2013-02-22 09:30:34 +0800

    第4章 字符串和格式化输入/输出 程序清单4.16 - 4.17

---
+++ chapter04/list4.16_varwidth.c
@@ -0,0 +1,20 @@
+﻿/* varwidth.c -- 使用可变宽度的输出字段 */
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned width, precision;
+	int number = 256;
+	double weight = 242.5;
+	
+	printf("What field width?\n");
+	scanf("%d", &width);
+	printf("The number is: %*d\n", width, number);
+	printf("Now enter a width and a precision: \n");
+	scanf("%d %d", &width, &precision);
+	printf("Weight = %*.*f\n", width, precision, weight);
+	printf("Done!");
+	
+	getch();
+	return 0;
+}
---
+++ chapter04/list4.17_skip2.c
@@ -0,0 +1,14 @@
+﻿/* skip2.c -- 跳过输入的头两个整数 */
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	
+	printf("Please enter three integers: \n");
+	scanf("%*d %*d %d", &n);
+	printf("The last integer was %d\n", n);
+	
+	getch();
+	return 0;
+}
